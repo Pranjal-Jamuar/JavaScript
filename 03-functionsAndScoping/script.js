b9bb970f1523f2f6f54d3b8c3d515dc7e653052b
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-01-27 08:34:46 +0530

    Functions code added

---
+++ 03-functionsAndScoping/script.js
@@ -0,0 +1,15 @@
+//** Normal functions
+// function sayHi() {
+//   console.log("Hi")
+//   console.log("bye")
+// }
+
+// sayHi()
+
+//! Exercise
+
+function sayName() {
+  console.log("Pranjal Jamuar")
+}
+
+sayName()

e03fa51d7961f0df1fe24d1608480b8e5ba8c985
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-01-31 06:37:06 +0530

    Id and Class Selectors

--- 05-basicDOM/script.js
+++ 05-basicDOM/script.js
@@ -9,3 +9,14 @@ console.log(document)
 const element = document.createElement("span")
 element.innerText = "Hello World"
 document.body.appendChild(element)
+
+//** id and class selectors
+
+const divWithId = document.getElementById("div-id")
+divWithId.style.color = "red"
+
+const divsWithClass = document.getElementsByClassName("div-class")
+
+const divsWithClassArray = Array.from(divsWithClass)
+
+divsWithClassArray.forEach(div => (div.style.color = "green"))

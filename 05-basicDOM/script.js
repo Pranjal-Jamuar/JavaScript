a494dcbfcd7b87b8be42919be039278d2737680d
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-01-31 06:26:01 +0530

    window and document object

---
+++ 05-basicDOM/script.js
@@ -0,0 +1,11 @@
+//* window object
+console.log(window)
+
+window.console.log("hi")
+
+//* document object
+console.log(document)
+
+const element = document.createElement("span")
+element.innerText = "Hello World"
+document.body.appendChild(element)

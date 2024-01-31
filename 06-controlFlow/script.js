9c5fddcd89feef2d21d94272f867eb8457a84c96
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-01-31 10:17:11 +0530

    New Section Added
    Control Flow section added
    If statement added in control flow

---
+++ 06-controlFlow/script.js
@@ -0,0 +1,33 @@
+//** if statement
+
+const userIsLoggedIn = false
+
+if (userIsLoggedIn) {
+  console.log("User logged in")
+} else {
+  console.log("Please Login")
+}
+
+const totalPrice = 100
+
+if (totalPrice == null) {
+  console.log("There is no total price")
+} else if (totalPrice < 50) {
+  console.log("Price less than 50")
+} else {
+  console.log("Price greater than 50")
+}
+
+//! Exercise
+
+const arrayList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+if (arrayList.length === 0) {
+  console.log("empty")
+} else if (arrayList.length < 5) {
+  console.log("small")
+} else if (arrayList.length < 10) {
+  console.log("medium")
+} else {
+  console.log("large")
+}

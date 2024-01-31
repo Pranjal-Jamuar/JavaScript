834d801b9ea2e9520838e2e68bf7c4be1516b139
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-01-31 11:10:43 +0530

    For Loop Added

--- 06-controlFlow/script.js
+++ 06-controlFlow/script.js
@@ -98,3 +98,15 @@ switch (number) {
     console.log("try again")
     break
 }
+
+//** For loop
+
+for (let i = 0; i < 5; i++) {
+  console.log("hi")
+}
+
+//! Exercise
+for (let i = 0; i <= 10; i++) {
+  if (i === 5) break
+  console.log(i)
+}
